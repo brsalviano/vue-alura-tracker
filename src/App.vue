4a08a350a830ee7dbfd54a5acf8817cc1ccfa088
Bruno Salviano da Silva Teixeira <bruno.salviano@gmail.com>
2022-06-05 15:07:46 -0300

    criando componente tarefa

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,33 @@
     </div>
     <div class="column is-three-quarter">
       <Formulario />
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue'
-import Formulario from './components/Formulario.vue'
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
+import Tarefa from "./components/Tarefa.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     BarraLateral,
-    Formulario
-  }
+    Formulario,
+    Tarefa,
+  },
 });
 </script>
 
 <style>
+.lista {
+  padding: 1.25rem;
+}
 </style>

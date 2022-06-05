96afd3c70500a259289f40614713e22b95cd6fac
Bruno Salviano da Silva Teixeira <bruno.salviano@gmail.com>
2022-06-05 17:10:08 -0300

    aplicando estilo fazendo bind de style

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,19 +1,21 @@
 <template>
-   <div class="box has-text-weight-bold">
-       <slot></slot>
-    </div> 
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot></slot>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "Box"
+  name: "Box",
+  data() {
+    return {
+      estilos: {
+        background: "#faf0ca",
+      },
+    };
+  },
 });
 </script>
 
-<style scoped>
-.box {
-  background: #faf0ca;
-}
-</style>

56f0715f0435a784a7c5b1f7f643a1da1ce8703f
Bruno Salviano da Silva Teixeira <bruno.salviano@gmail.com>
2022-06-05 16:36:00 -0300

    renderização condicional do Box em App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
     <div class="column is-three-quarter">
       <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Tarefa
+          v-for="(tarefa, index) in tarefas"
+          :key="index"
+          :tarefa="tarefa"
+        />
+        <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje :( </Box>
       </div>
     </div>
   </main>
@@ -14,10 +19,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import ITarefa from "./interfaces/ITarefa";
 import BarraLateral from "./components/BarraLateral.vue";
 import Formulario from "./components/Formulario.vue";
 import Tarefa from "./components/Tarefa.vue";
-import ITarefa from "./interfaces/ITarefa";
+import Box from "./components/Box.vue";
 
 export default defineComponent({
   name: "App",
@@ -25,11 +31,17 @@ export default defineComponent({
     BarraLateral,
     Formulario,
     Tarefa,
+    Box,
   },
   data() {
     return {
       tarefas: [] as ITarefa[],
     };
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
+    },
   },
   methods: {
     salvarTarefa(tarefa: ITarefa) {
